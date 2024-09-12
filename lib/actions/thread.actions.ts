e49a820601486c05ff61ead43b39b72b77f4341e
Hunter Wang <s092284s@gmail.com>
2024-09-12 09:11:41 +0800

    fetching threads

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -36,3 +36,42 @@ export async function createThread({
     throw new Error(`Error creating Thread: ${error.message}`);
   }
 }
+
+export async function fetchThreads(pageNumber = 1, pageSize = 20) {
+  try {
+    connectToDB();
+
+    // caculate the number of threads to skip
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    // fetch the threads that have no parents
+    const threadsQuery = Thread.find({
+      parentId: { $in: [null, undefined] },
+    })
+      .sort({ createdAT: "desc" })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({ path: "author", model: User })
+      .populate({
+        path: "children",
+        populate: {
+          path: "author",
+          model: User,
+          select: "_id name parentId image",
+        },
+      });
+
+    // Count the total number of top-level posts (threads) i.e., threads that are not comments.
+    const totalThreadsCount = await Thread.countDocuments({
+      parentId: { $in: [null, undefined] },
+    });
+
+    const threads = await threadsQuery.exec();
+
+    const isNext = totalThreadsCount > skipAmount + threads.length;
+
+    return { threads, isNext };
+  } catch (error: any) {
+    throw new Error(`Error fetching threads: ${error.message}`);
+  }
+}
